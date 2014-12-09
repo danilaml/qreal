e23ae188790a5f4752a6b8844836a46154fe75d7
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2014-12-10 00:14:50 +0300

    Added tests for boolean and nil.

--- qrtest/unitTests/qrtextTests/luaHMSemanticAnalyzerTest.cpp
+++ qrtest/unitTests/qrtextTests/luaHMSemanticAnalyzerTest.cpp
@@ -12,6 +12,10 @@
 #include "qrtext/lua/types/float.h"
 #include "qrtext/lua/types/string.h"
 #include "qrtext/lua/types/table.h"
+#include "qrtext/lua/types/boolean.h"
+#include "qrtext/lua/types/function.h"
+#include "qrtext/lua/types/nil.h"
+#include "qrtext/lua/types/number.h"
 
 #include "gtest/gtest.h"
 
@@ -59,13 +63,24 @@ TEST_F(LuaHMSemanticAnalyzerTest, string)
 	EXPECT_TRUE(mAnalyzer->type(tree)->is<String>());
 }
 
-//TEST_F(LuaHMSemanticAnalyzerTest, booleanCheck)
-//{
-//	auto tree = parse("1 == 1");
-//	mAnalyzer->analyze(tree);
-//	EXPECT_TRUE(mErrors.empty());
-//	EXPECT_TRUE(mAnalyzer->type(tree)->is<Boolean>());
-//}
+TEST_F(LuaHMSemanticAnalyzerTest, booleanCheck)
+{
+	auto treeTrue = parse("true");
+	auto treeFalse = parse("false");
+	mAnalyzer->analyze(treeTrue);
+	mAnalyzer->analyze(treeFalse);
+	EXPECT_TRUE(mErrors.empty());
+	EXPECT_TRUE(mAnalyzer->type(treeTrue)->is<Boolean>());
+	EXPECT_TRUE(mAnalyzer->type(treeFalse)->is<Boolean>());
+}
+
+TEST_F(LuaHMSemanticAnalyzerTest, nil)
+{
+	auto tree = parse("nil");
+	mAnalyzer->analyze(tree);
+	EXPECT_TRUE(mErrors.empty());
+	EXPECT_TRUE(mAnalyzer->type(tree)->is<Nil>());
+}
 
 //TEST_F(LuaHMSemanticAnalyzerTest, assignment)
 //{
